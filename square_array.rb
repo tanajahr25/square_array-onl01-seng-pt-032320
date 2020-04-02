d6a561e89a7cb2a6224354d37bbfb0008c639cc9
Tanajah Rosenbaum <tanajahr25@gmail.com>
2020-04-02 04:03:31 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,6 @@
 def square_array(array)
   array2 = []
+<<<<<<< HEAD
   array.each { |number| array2.push(number ** 2)}
   return array2
 end
@@ -7,3 +8,12 @@ end
 # def square_array(array)
 #   array.collect { |number| number ** 2} 
 # end
+=======
+array.each do |numbers|
+  
+# array2.push("numbers * numbers")
+return array2
+array2.inspect 
+end
+end
+>>>>>>> 5da8d2d001c46bd20d6f6c2fc280febe7bcc35a7
